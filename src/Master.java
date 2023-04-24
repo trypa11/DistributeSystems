8cd4a4ca0e39d0f5805285063545e9585d81fb41
trypa11 <67585616+trypa11@users.noreply.github.com>
2023-04-25 00:14:20 +0300

    worker chunks

--- src/Master.java
+++ src/Master.java
@@ -8,45 +8,75 @@ import java.lang.*;
 public class Master {
     ServerSocket s;
     Socket providerSocket;
-    private int file_num=1;
+    private int file_num = 1;
 
-    public ArrayList<Waypoint> Map( ArrayList<Waypoint> waypoints) {
+    public ArrayList<Waypoint> Map(ArrayList<Waypoint> waypoints) throws Exception {
         ArrayList<Waypoint> map = new ArrayList<Waypoint>();
         GPXParser parser = new GPXParser("C:\\Users\\user\\Desktop\\route1.gpx");
         map = parser.getWaypoints();
         for (int i = 0; i < waypoints.size(); i++) {
-            waypoints.get(i).setId(file_num*1000+i);
-            map.add( waypoints.get(i));
+            waypoints.get(i).setId(file_num * 1000 + i);
+            map.add(waypoints.get(i));
         }
         file_num++;
         return map;
 
     }
 
-// call worker thread to handle the request
-    
-/*public static void main(String[] args) {
-    Worker w1 = new Worker(1,2);
-    Worker w2 = new Worker(3,4);
-    w1.start();
-    w2.start();
-}*/
+    // call worker thread to handle the request
+
+    public static void main(String[] args) throws Exception {
+        // Load waypoints from GPX file
+        ArrayList<Waypoint> map = new ArrayList<Waypoint>();
+        GPXParser parser = new GPXParser("D:\\Downloads\\gpxs\\gpxs\\route1.gpx");
+        map = parser.getWaypoints();
+        // Split waypoint list into chunks
+        int numChunk = 4;
+        int chunkSize = map.size() / numChunk;
+        ArrayList<ArrayList<Waypoint>> chunks = new ArrayList<ArrayList<Waypoint>>();
+        for (int i = 0; i < numChunk; i++) {
+            ArrayList<Waypoint> chunk = new ArrayList<Waypoint>();
+            for (int j = 0; j < chunkSize; j++) {
+                chunk.add(map.get(i * chunkSize + j));
+            }
+            chunks.add(chunk);
+        }
+        // round robin sheduling for the chunks to the workers
+        int worker_num = 1;
+        for (int i = 0; i < numChunk; i++) {
+            Thread w = new Worker(chunks.get(i));
+            w.start();
+            worker_num++;
+        }
+        // wait for all workers to finish
+        for (int i = 0; i < numChunk; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 
     void openServer() throws IOException {
         try {
 
             /* Create Server Socket */
-            s= new ServerSocket(4321, 10);
+            s = new ServerSocket(4321, 10);
 
             while (true) {
+
                 /* Accept the connection */
-                providerSocket= s.accept();
+                providerSocket = s.accept();
                 /* Handle the request */
                 Thread d = new ActionForWorkers(providerSocket);
                 d.start();
             }
 
-        } catch (IOException ioException) {
+        } catch (
+
+                IOException ioException) {
             ioException.printStackTrace();
         } finally {
             try {
@@ -57,8 +87,6 @@ public class Master {
         }
 
     }
-
-
 
 }
 
